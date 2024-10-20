682554fca3f45a1f55ce36745152bb19e8ef25b7
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-10-20 21:26:35 +0100

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+
+
